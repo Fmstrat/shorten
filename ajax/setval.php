85f39edfa016011172f873db864f335786e54b88
Fmstrat <github@nosolutions.com>
2015-01-21 15:03:20 -0500

    Bugfix for error log

--- ajax/setval.php
+++ ajax/setval.php
@@ -3,15 +3,15 @@
 OCP\User::checkAdminUser();
 \OCP\App::checkAppEnabled('shorten');
 
-if ($_POST['host']) {
+if (isset($_POST['host'])) {
 	OCP\Config::setAppValue('shorten', 'host', $_POST['host']);
 	echo "host:".$_POST['host'];
 }
-if ($_POST['api']) {
+if (isset($_POST['api'])) {
 	OCP\Config::setAppValue('shorten', 'api', $_POST['api']);
 	echo "api:".$_POST['api'];
 }
-if ($_POST['type']) {
+if (isset($_POST['type'])) {
 	OCP\Config::setAppValue('shorten', 'type', $_POST['type']);
 	echo "type:".$_POST['type'];
 }
