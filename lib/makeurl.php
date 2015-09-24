1d2b7a507fdbca71bfe62811a332d51d0abbf197
wilsonlmh <lu6ni4z-forum@Yahoo.com.hk>
2015-09-24 19:08:56 +0800

    aad SQL column 'type'

--- lib/makeurl.php
+++ lib/makeurl.php
@@ -18,7 +18,7 @@ function rand_chars($length) {
 
 function getShortcode($url) {
 	$shortcode = '';
-	$query = OCP\DB::prepare('SELECT shortcode FROM *PREFIX*shorten WHERE url=?');
+	$query = OCP\DB::prepare('SELECT shortcode FROM *PREFIX*shorten WHERE type=\'internal\' AND url=?');
 	$results = $query->execute(Array($url))->fetchAll();
 	if ($results) {
 		foreach($results as $result) {
@@ -29,12 +29,12 @@ function getShortcode($url) {
 		$shortcode = rand_chars(6);
 		$found = true;
 		while ($found) {
-			$query = OCP\DB::prepare('SELECT id FROM *PREFIX*shorten WHERE shortcode=?');
+			$query = OCP\DB::prepare('SELECT id FROM *PREFIX*shorten WHERE type=\'internal\' AND shortcode=?');
 			$results = $query->execute(Array($shortcode))->fetchAll();
 			if (!$results) {
 				$found = false;
 				$uid = \OCP\User::getUser();
-				$query = OCP\DB::prepare('INSERT INTO *PREFIX*shorten (uid, shortcode, url) VALUES (?,?,?)');
+				$query = OCP\DB::prepare('INSERT INTO *PREFIX*shorten (uid, shortcode, url, type) VALUES (?,?,?,\'internal\')');
 				$query->execute(Array($uid,$shortcode,$url));
 				$id = OCP\DB::insertid('*PREFIX*shorten');
 			} else
@@ -72,32 +72,51 @@ function generateUrl() {
 		}
 	} elseif ($type == "custom") {
 		$ret = $curUrl; //Failover
-		//use CURL to query
-		$query = sprintf($customUrl,urlencode($curUrl));
-		if (!((in_array(parse_url($query, PHP_URL_SCHEME),array('http','https')))) && (filter_var($query, FILTER_VALIDATE_URL))) { $query = ''; }
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, $query); 
-		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
-	        $raw = curl_exec($ch); 
-	        curl_close($ch);
-	        //OK, parse the JSON
-	        $customJSON = preg_replace("/[^a-zA-Z0-9\-\>]/", "", $customJSON); //remove unwanted characters due to security reason
-	        ob_start();
-	        ob_flush();
-	        eval('$veryVeryVeryVeryVeryVeryVeryLoooongLoooongLoooongNameOfVarOfJSON = json_decode("' . addslashes($raw) . '");'); //use long name due to security reason
-	        ob_clean();
-		$url = eval('return $veryVeryVeryVeryVeryVeryVeryLoooongLoooongLoooongNameOfVarOfJSON' . $customJSON . ';');
-		 ob_get_contents();
-		ob_end_clean();
+		//Check for exist
+		$shortcode = '';
+		$query = OCP\DB::prepare('SELECT shortcode FROM *PREFIX*shorten WHERE type=\'custom\' AND url=?');
+		$results = $query->execute(Array($curUrl))->fetchAll();
+		if ($results) {
+			foreach($results as $result) {
+				$shortcode = $result['shortcode'];	
+			}
+		}
+		if ($shortcode == '') {
+			//use CURL to query
+			$query = sprintf($customUrl,urlencode($curUrl));
+			if (!((in_array(parse_url($query, PHP_URL_SCHEME),array('http','https')))) && (filter_var($query, FILTER_VALIDATE_URL))) { $query = ''; }
+			$ch = curl_init();
+			curl_setopt($ch, CURLOPT_URL, $query); 
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+	        	$raw = curl_exec($ch); 
+	        	curl_close($ch);
+	        	//OK, parse the JSON
+	        	$customJSON = preg_replace("/[^a-zA-Z0-9\-\>]/", "", $customJSON); //remove unwanted characters due to security reason
+	        	ob_start();
+	        	ob_flush();
+	        	eval('$veryVeryVeryVeryVeryVeryVeryLoooongLoooongLoooongNameOfVarOfJSON = json_decode("' . addslashes($raw) . '");'); //use long name due to security reason
+	        	ob_clean();
+			$url = eval('return $veryVeryVeryVeryVeryVeryVeryLoooongLoooongLoooongNameOfVarOfJSON' . $customJSON . ';');
+		 	ob_get_contents();
+			ob_end_clean();
 		
-		//Debug only
-            	OCP\Util::writeLog('shorten',"\$raw=$raw",0);
-            	OCP\Util::writeLog('shorten',"\$url=$url",0);   
+			//Debug only
+            		OCP\Util::writeLog('shorten',"\$raw=$raw",0);
+            		OCP\Util::writeLog('shorten',"\$url=$url",0);   
             	
-            	//Finally output url after check if URL is valid
-            if ((in_array(parse_url($url, PHP_URL_SCHEME),array('http','https'))) && (filter_var($url, FILTER_VALIDATE_URL))) { $ret = $url; } 
-            
+            		//Finally output url after check if URL is valid
+            		if ((in_array(parse_url($url, PHP_URL_SCHEME),array('http','https'))) && (filter_var($url, FILTER_VALIDATE_URL))) {
+            			$found = false;
+				$uid = \OCP\User::getUser();
+				$query = OCP\DB::prepare('INSERT INTO *PREFIX*shorten (uid, shortcode, url, type) VALUES (?,?,?,\'custom\')');
+				$query->execute(Array($uid,$url,$curUrl));
+				$id = OCP\DB::insertid('*PREFIX*shorten');
+            			$ret = $url;
+            		} 
+		} else {
+			$ret = $shortcode
+		}
 	}else {
 		$ret = $curUrl;
 	}
